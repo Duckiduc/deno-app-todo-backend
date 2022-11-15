6f15de99ffe90e0e23673548bad0f3bb4a742c3d
Duc-Thomas <duckiduc.dev@gmail.com>
2022-11-15 10:56:32 +0100

    Add model for database

---
+++ models/todos_models.ts
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from "https://deno.land/x/denodb@v1.1.0/mod.ts";
+
+import { getDB } from "../utils/db_utils.ts";
+
+class Todo extends Model {
+  static fields = {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    text: DataTypes.STRING,
+  };
+  static table = "todo";
+}
+
+const db = getDB();
+
+try {
+  db.link([Todo]);
+  await db.sync();
+} catch (err) {
+  console.log(err);
+}
+
+export default Todo;
